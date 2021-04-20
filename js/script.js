69f0b8096a26ffee8b888e364e87c6da5a51c9c9
kkozemjakins <kkozemjakins@gmail.com>
2021-04-20 16:52:31 +0300

    third commit soon will be finished

--- js/script.js
+++ js/script.js
@@ -1,29 +1,44 @@
 console.log("rabotaet");
 
+let listas = []
+
 document.getElementById("submit").addEventListener('click', newList);
 
 
 function newList(){
 
-    if (product.value == '') {
+    if (product.value === '') {
         alert('You must write name of product')
     };
 
-    if (amount.value == '') {
+    if (amount.value === '') {
         alert('You must write amount of product')
     };
 
 
     let list = {product: product.value, amount: amount.value};
 
+    console.log(list);
+
     product.value = "";
     amount.value = "";
 
-    console.log(list);
+    listas.push(list);
+
+    render()
+};
+
+function render() {
+    let  = document.getElementById('myLIST');
+    stroka_v_ul.innerHTML = "";
+
+    for(let i = 0; i < list.length; i++) {
+        let li = `
+        <li id="stroka_v_ul">Product:${stroka_v_list[i].product}    Amount:${stroka_v_list[i].amount} </li>`;
+
+        list.innerHTML += stroka_v_list;
+    }
 
 
-    var li = document.createElement("li");
-    var t = document.createTextNode(inputValue);
-
-   
-}
+    localStorage.setItem("list",JSON.stringify(stroka_v_list))
+};
