b8f64de3d48c5fbfdb8d4b7c90edd893848d459c
kkozemjakins <kkozemjakins@gmail.com>
2021-04-20 16:12:35 +0300

    javascript alert

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 console.log("rabotaet");
 
-document.getElementById("submit").addEventListener('click', Button);
+document.getElementById("submit").addEventListener('click', newList);
 
-function Button() {
-    console.log("Nazal knopku");
+
+function newList(){
+
+    if (product.value == '') {
+        alert('You must write name of product')
+    };
+
+    if (amount.value == '') {
+        alert('You must write amount of product')
+    };
+
 
     let list = {product: product.value, amount: amount.value};
 
     product.value = "";
     amount.value = "";
 
-    console.log(list)
+    console.log(list);
+
+
+    var li = document.createElement("li");
+    var t = document.createTextNode(inputValue);
+
+   
 }
