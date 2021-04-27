7509969e70047c2e5e24f3a897d14b76fdeb506f
kkozemjakins <kkozemjakins@gmail.com>
2021-04-27 16:45:51 +0300

    add button delete

--- js/script.js
+++ js/script.js
@@ -8,10 +8,16 @@ window.addEventListener('load', () => {
     render();
 });
 
-window.addEventListener('load', () => {
-    console.log(listas_js);
-    render();
-});
+const list = document.querySelector('#div_ul')
+
+list.addEventListener('click', (e) => {
+    if(e.target.className == 'btn_delete'){
+      const li = e.target.parentElement;
+      li.parentNode.removeChild(li);
+      listas_js.splice(li, 1);
+      localStorage.setItem('listas_js',JSON.stringify(listas_js));
+    }
+  });
 
 function render() {
     let stroka_v_ul = document.getElementById('div_ul');
@@ -19,8 +25,13 @@ function render() {
 
     for(let i = 0; i < listas_js.length; i++) {
         let list = `
-        <li id="stroka_v_ul">Product:${listas_js[i].product}<button id="btn_delete">X</button></li>
-        <li id="nd_stroka_v_ul">Amount:${listas_js[i].amount} </li>`;
+        <li class="stroka_v_ul">
+            <div>
+                <p>Product:${listas_js[i].product}</p>
+                <p id="nd_stroka_v_ul">Amount:${listas_js[i].amount}</p>
+                <button class="btn_delete">X</button>
+            </div>
+        </li>`;
 
         stroka_v_ul.innerHTML += list;
     };
